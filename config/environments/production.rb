412e223a317804eb710dea189bedb2a153c9c33c
Davidzhu001 <qluoluo@icloud.com>
2015-06-30 12:59:05 -0500

    make it better

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'bethanyprayer.herokuapp.com'
+  host = 'gggobble.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
